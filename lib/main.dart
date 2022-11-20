f7766474c909cd398db30f8a82f05a3f4b0f1b00
Emotionalboy_SY <yoppyopp@naver.com>
2022-11-20 23:57:05 +0900

    delete unused importations

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'LoginPage.dart';
-import 'RegisterPage.dart';
-import 'SuccessRegister.dart';
 import 'ChatPage.dart';
 
 void main() async {
